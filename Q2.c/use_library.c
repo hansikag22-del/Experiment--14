b1899e13f73e747250bd24dd69bc805598caf699
Hansika Gupta <hansikag22@gmail.com>
2025-11-28 20:14:00 +0530

    Add files via upload

--- Q2.c/use_library.c
+++ Q2.c/use_library.c
@@ -1,1 +1,15 @@
+// use_library.c
+#include <stdio.h>
+#include "arith.h"   // Include the header of the static library
 
+int main() {
+    int x = 15, y = 5;
+
+    printf("Using Static Library Functions:\n");
+    printf("Add: %d\n", add(x, y));
+    printf("Sub: %d\n", sub(x, y));
+    printf("Mul: %d\n", mul(x, y));
+    printf("Div: %d\n", divide(x, y));
+
+    return 0;
+}
